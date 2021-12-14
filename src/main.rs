23f02591d76134dd58350d50e5191d4656f86f59
Rob Elsner <thatsnotright@gmail.com>
2021-12-13 20:50:52 -0700

    feat: wrap

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,34 @@
-extern crate rand;
-extern crate sdl2;
-
+use rand::Rng;
 use sdl2::event::Event;
 use sdl2::keyboard::Keycode;
 use sdl2::pixels::Color;
-use std::mem::size_of;
+use std::ops::Range;
 
+#[derive(Debug)]
 struct Pos {
   point: sdl2::rect::Point,
   flame: Color,
 }
 
 fn create_random_start(w: u32, h: u32) -> Pos {
+  let mut rng = rand::thread_rng();
+
   Pos {
-    point: sdl2::rect::Point::new(rand::random(), rand::random()),
-    flame: Color::RGB(rand::random(), rand::random(), rand::random()),
+    point: sdl2::rect::Point::new(
+      rng.gen_range(Range {
+        start: 0,
+        end: w as i32,
+      }),
+      rng.gen_range(Range {
+        start: 0,
+        end: h as i32,
+      }),
+    ),
+    flame: Color::RGB(
+      rng.gen_range(Range { start: 0, end: 255 }),
+      rng.gen_range(Range { start: 0, end: 255 }),
+      rng.gen_range(Range { start: 0, end: 255 }),
+    ),
   }
 }
 
@@ -23,6 +37,7 @@ pub fn main() -> Result<(), String> {
   const h: u32 = 600;
   let sdl_context = sdl2::init()?;
   let video_subsystem = sdl_context.video()?;
+  let mut rng = rand::thread_rng();
 
   let window = video_subsystem
     .window("rust-sdl2 demo: Window", w, h)
@@ -38,17 +53,15 @@ pub fn main() -> Result<(), String> {
   let mut tick = 0;
 
   let mut event_pump = sdl_context.event_pump().map_err(|e| e.to_string())?;
-
-  const flames: [&Pos; 8] = [
-    &create_random_start(w, h),
-    &create_random_start(w, h),
-    &create_random_start(w, h),
-    &create_random_start(w, h),
-    &create_random_start(w, h),
-    &create_random_start(w, h),
-    &create_random_start(w, h),
-    &create_random_start(w, h),
-  ];
+  let mut flames: std::vec::Vec<Pos> = std::vec::Vec::new();
+  flames.push(create_random_start(w, h));
+  flames.push(create_random_start(w, h));
+  flames.push(create_random_start(w, h));
+  flames.push(create_random_start(w, h));
+  flames.push(create_random_start(w, h));
+  flames.push(create_random_start(w, h));
+  flames.push(create_random_start(w, h));
+  flames.push(create_random_start(w, h));
 
   'running: loop {
     for event in event_pump.poll_iter() {
@@ -66,13 +79,24 @@ pub fn main() -> Result<(), String> {
       tick += 1;
     }
 
-    for f in flames.iter() {
+    for f in &mut flames {
+      f.point.x -= rng.gen_range(Range { start: -1, end: 2 });
+      f.point.y -= rng.gen_range(Range { start: -1, end: 2 });
+      if f.point.x < 0 {
+        f.point.x = w as i32;
+      }
+      if f.point.x >= w as i32 {
+        f.point.x = 0;
+      }
+      if f.point.y < 0 {
+        f.point.y = h as i32;
+      }
+      if f.point.y >= h as i32 {
+        f.point.y = 0;
+      }
       canvas.set_draw_color(f.flame);
       canvas.draw_point(f.point);
     }
-
-    // canvas.set_draw_color(Color::RGB(0, 0, 0));
-    // canvas.clear();
     canvas.present();
   }
 
